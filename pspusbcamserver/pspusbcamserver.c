365d237187bf42635672495f53e68ace62761858
Jacky (Hansjorg) Baltes <jacky@arisu.robotics.com>
2008-08-28 08:07:18 -0500

    Finished simple version of the server

---
+++ pspusbcamserver/pspusbcamserver.c
@@ -0,0 +1,216 @@
+ /*
+ * PSP Software Development Kit - http://www.pspdev.org
+ * -----------------------------------------------------------------------
+ * Licensed under the BSD license, see LICENSE in PSPSDK root for details.
+ *
+ * main.c - Camera Test example
+ *
+ * Copyright (c) 2008 Jacky Baltes <jacky@cs.umanitoba.ca>
+ *
+ * $Id: main.c 705 2005-07-20 18:09:56Z tyranid $
+ */
+
+#include <pspkernel.h>
+#include <pspdebug.h>
+#include <pspdisplay.h>
+#include <pspctrl.h>
+#include <pspusb.h>
+#include <pspusbcam.h>
+#include <pspusbacc.h>
+#include <psputility_usbmodules.h>
+#include <pspsircs.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "pspusbcamserver.h"
+
+/* Define the module info section */
+PSP_MODULE_INFO("CAMERA TEST", 0, 1, 0);
+
+/* Define the main thread's attribute value (optional) */
+PSP_MAIN_THREAD_ATTR( THREAD_ATTR_USER );
+PSP_HEAP_SIZE_KB(15*1024);
+PSP_MAIN_THREAD_STACK_SIZE_KB(512);
+
+/* Prototypes */
+int unloadCameraModules( void );
+int loadCameraModules( void );
+int startCamera( void );
+
+static LogFunction logFunction = 0;
+
+int
+module_start (SceSize argc, void* argp)
+{
+  return SCE_KERNEL_ERROR_OK;
+}
+
+int 
+module_stop()
+{
+  return SCE_KERNEL_ERROR_OK;
+}
+
+
+int 
+loadCameraModules( void )
+{
+  int err = SCE_KERNEL_ERROR_OK;
+
+  logFunction("Loading PSP_USB_MODULE_ACC ...");
+  if ( ( err = sceUtilityLoadUsbModule(PSP_USB_MODULE_ACC) ) == 0 )
+    {
+      logFunction("success\n");
+    }
+  else
+    {
+      logFunction("ERROR %08x\n", err );
+      for(;;)
+	{
+	  sceDisplayWaitVblankStart(); 
+	}
+      goto exit;
+    }
+
+  logFunction("Loading PSP_USB_MODULE_CAM ...");
+  if ( ( err = sceUtilityLoadUsbModule(PSP_USB_MODULE_CAM) ) == 0 )
+    {
+      logFunction("success\n");
+    }
+  else
+    {
+      logFunction("ERROR %08x\n", err );
+      for(;;)
+	{
+	  sceDisplayWaitVblankStart(); 
+	}
+      goto exit;
+    }
+
+ exit:
+  return err;
+}
+
+int
+startCamera( void )
+{
+  int err = SCE_KERNEL_ERROR_OK;
+
+  logFunction("Starting PSP_USBBUS ...");
+  if ( ( err = sceUsbStart(PSP_USBBUS_DRIVERNAME, 0, 0) == 0 ) )
+    {
+      logFunction("success\n");
+    }
+  else
+    {
+      logFunction("Error 0x%08X starting usbbus driver.\n", err);
+      goto exit;
+    }
+
+  logFunction("Starting PSP_USBACC ...");
+  if ( ( err = sceUsbStart(PSP_USBACC_DRIVERNAME, 0, 0) ) == 0 )
+    {
+      logFunction("success\n");
+    }
+  else
+    {
+      logFunction("Error 0x%08X starting usbacc driver.\n", err);
+      goto exit;
+    }
+  
+  logFunction("Starting PSP_USBCAM ...");
+  if ( ( err = sceUsbStart(PSP_USBCAM_DRIVERNAME, 0, 0) ) == 0 )
+    {
+      logFunction("success\n");
+    }
+  else
+    {
+      logFunction("Error 0x%08X starting usbcam driver.\n", err);
+      goto exit;
+    }
+
+  logFunction( "Activating PSP_USBCAM ..." );
+  if ( ( err = sceUsbActivate(PSP_USBCAM_PID) ) == 0 )
+    {
+      logFunction("success\n");
+    }
+  else
+    {
+      logFunction("Error 0x%08X activating usbcam driver.\n", err);
+      goto exit;
+    }
+
+ exit:
+  return err;
+}
+
+int 
+unloadCameraModules( void )
+{
+  int err = SCE_KERNEL_ERROR_OK;
+
+  logFunction("Unloading PSP_USB_MODULE_ACC ...");
+  if ( ( err = sceUtilityUnloadUsbModule(PSP_USB_MODULE_ACC) ) == 0 )
+    {
+      logFunction("success\n");
+    }
+  else
+    {
+      logFunction("ERROR %08x\n", err );
+      for(;;)
+	{
+	  sceDisplayWaitVblankStart(); 
+	}
+      goto exit;
+    }
+
+  logFunction("Unloading PSP_USB_MODULE_CAM ...");
+  if ( ( err = sceUtilityUnloadUsbModule(PSP_USB_MODULE_CAM) ) == 0 )
+    {
+      logFunction("success\n");
+    }
+  else
+    {
+      logFunction("ERROR %08x\n", err );
+      for(;;)
+	{
+	  sceDisplayWaitVblankStart(); 
+	}
+      goto exit;
+    }
+
+ exit:
+  return err;
+}
+
+int 
+PspUsbCam_Server_Initialize( LogFunction logFunc )
+{
+  logFunction = logFunc;
+
+  loadCameraModules();
+  startCamera();
+
+  logFunction ("Camera Test Application by Jacky\n");
+  logFunction ("-------------------------------\n");
+
+  return SCE_KERNEL_ERROR_OK;
+}
+
+void 
+showLogMessage(const char * fmt, ...)
+{
+  char msg[256];
+  
+  va_list argp;
+
+  va_start(argp, fmt);
+  
+  vsnprintf(msg, sizeof(msg), fmt, argp);
+
+  va_end(argp);
+  
+  logFunction( msg );
+}
---
+++ pspusbcamserver/pspusbcamserver.h
@@ -0,0 +1,28 @@
+
+/*
+ * PSP Software Development Kit - http://www.pspdev.org
+ * -----------------------------------------------------------------------
+ * Licensed under the BSD license, see LICENSE in PSPSDK root for details.
+ *
+ * pspusbcam_server.h - Specifies the Interface for the PSP USB Cam server
+ *
+ * Copyright (c) 2008 Jacky Baltes <jacky@cs.umanitoba.ca>
+ *
+ * $Id: main.c 705 2005-07-20 18:09:56Z tyranid $
+ */
+
+#ifndef _PSP_USB_CAM_SERVER_H_
+#define _PSP_USB_CAM_SERVER_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+  /** Defines the gui function callback */
+  typedef int (*LogFunction)( char * msg, ... );
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _PSP_USB_CAM_SERVER_H_ */
